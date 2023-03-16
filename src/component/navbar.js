1e779e134c4d0126166c0e2e05e12f31397955a9
Apriana <aprianacahya04@gmail.com>
2023-03-16 10:40:00 +0700

    Update navbar.js

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route}   from 'react-router-dom';
+import {HashRouter as Router, Routes, Route}   from 'react-router-dom';
 import PageHome from '../pages/home';
 import PageAbout from '../pages/about';
 
